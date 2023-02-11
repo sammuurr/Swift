3d2483206594d24680dc08654ca0c129eaadd739
Samur Quseionov <sammuurr@gmail.com>
2023-02-11 23:56:39 +0300

    Initial Commit

---
+++ projekt/projektApp.swift
@@ -0,0 +1,17 @@
+//
+//  projektApp.swift
+//  projekt
+//
+//  Created by Самур Гусейнов on 11.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct projektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
